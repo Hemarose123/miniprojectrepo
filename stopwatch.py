ba17166ff313f67fd4d1c3be26c549b4c09b0dbc
Hemarose123 <163624741+Hemarose123@users.noreply.github.com>
2024-11-08 05:01:54 +0530

    python

---
+++ stopwatch.py
@@ -0,0 +1,68 @@
+import tkinter as Tkinter 
+from datetime import datetime
+counter = 66600
+running = False
+def counter_label(label): 
+    def count(): 
+        if running: 
+            global counter 
+    
+            if counter==66600:             
+                display="Starting..."
+            else:
+                tt = datetime.fromtimestamp(counter)
+                string = tt.strftime("%H:%M:%S")
+                display=string 
+   
+            label['text']=display   
+   
+            label.after(1000, count)  
+            counter += 1
+    
+    count()      
+   
+def Start(label): 
+    global running 
+    running=True
+    counter_label(label) 
+    start['state']='disabled'
+    stop['state']='normal'
+    reset['state']='normal'
+   
+ 
+def Stop(): 
+    global running 
+    start['state']='normal'
+    stop['state']='disabled'
+    reset['state']='normal'
+    running = False
+   
+ 
+def Reset(label): 
+    global counter 
+    counter=66600
+   
+     
+    if running==False:       
+        reset['state']='disabled'
+        label['text']='Welcome!'
+   
+    else:                
+        label['text']='Starting...'
+   
+root = Tkinter.Tk() 
+root.title("Stopwatch") 
+   
+
+root.minsize(width=600, height=100) 
+label = Tkinter.Label(root, text="Welcome!", fg="red", font="Verdana 40 bold") 
+label.pack() 
+f = Tkinter.Frame(root)
+start = Tkinter.Button(f, text='Start', width=20, command=lambda:Start(label)) 
+stop = Tkinter.Button(f, text='Stop',width=20,state='disabled', command=Stop) 
+reset = Tkinter.Button(f, text='Reset',width=20, state='disabled', command=lambda:Reset(label)) 
+f.pack(anchor = 'center',pady=5)
+start.pack(side="left") 
+stop.pack(side ="left") 
+reset.pack(side="left") 
+root.mainloop()
